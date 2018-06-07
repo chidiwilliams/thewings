e287a9effa0515d725c851b9cd957f395981004f
Williams Chidi <williamschidi1@gmail.com>
2018-06-07 22:04:39 +0100

    favicon resize

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
 
         <title>The Wings Awards</title>
 
-        <link rel="shortcut icon" href="{{ asset('img/wings_triangle_H80_icon.ico') }}" />
+        <link rel="shortcut icon" href="{{ asset('img/wings_triangle_0PE_icon.ico') }}" />
         <link href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans|Roboto" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         @yield('css')
