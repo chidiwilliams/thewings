ee0b5402924a51dfd026498410cc8994447db859
Williams Chidi <williamschidi1@gmail.com>
2018-07-02 20:46:19 +0100

    corrected Twitteer card

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="" />
         <meta name="twitter:title" content="The Wings Ceremony" />
         <meta name="twitter:description" content="The Wings Ceremony holds on Saturday 21 July, 2018. Alumni and members of the 2018 graduating class are all invited." />
