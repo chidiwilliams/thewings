eec4c03e4d798b2e9af3bcf943162bfb0b512242
Williams Chidi <williamschidi1@gmail.com>
2018-07-02 12:04:33 +0100

    CSS compiled

--- app/Http/Controllers/DoneController.php
+++ app/Http/Controllers/DoneController.php
@@ -9,4 +9,8 @@ class DoneController extends Controller
     public function done(Request $request) {
         return view('done');
     }
+
+    public function ceremony(Request $request) {
+        return view('ceremony');
+    }
 }
