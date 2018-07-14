db5682679e8046be35de96a761afd7bf82b42183
Williams Chidi <williamschidi1@gmail.com>
2018-07-14 13:02:16 +0100

    null

--- routes/web.php
+++ routes/web.php
@@ -38,5 +38,4 @@ Route::get('terms', 'TermsController@terms')->name('terms');
 Route::group(['middleware' => ['isAdmin']], function () {
     Route::get('admin/users', 'ExportController@users');
     Route::get('admin/nominations', 'ExportController@nominations');
-    Route::get('admin/ivs', 'DoneController@ivs');
 });
